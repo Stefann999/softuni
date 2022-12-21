c7083d93ab02cc49197e285a27b29b3c65d1122f
Stefann999 <stefchog25@gmail.com>
2022-12-21 20:26:01 +0200

    First, second, third tasks

---
+++ Exercise-Arrays/3. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+
+            int[] firstArr = new int[lenght];
+            int[] secondArr = new int[lenght];
+
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = numbers[0];
+                    secondArr[i] = numbers[1];
+                }
+                else
+                {
+                    firstArr[i] = numbers[1];
+                    secondArr[i] = numbers[0];
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", firstArr));
+            Console.WriteLine(String.Join(" ", secondArr));
+        }
+    }
+}
