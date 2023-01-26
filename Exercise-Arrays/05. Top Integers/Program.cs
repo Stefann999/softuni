6314b25f0913eb72f633296140ca16423a651bf5
Stefann999 <stefchog25@gmail.com>
2023-01-26 16:52:15 +0200

    Added project 5

---
+++ Exercise-Arrays/05. Top Integers/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+for (int i = 0; i < arr.Length - 1; i++)
+{
+    bool isTop = true;
+
+    for (int j = i + 1; j < arr.Length; j++)
+
+    if (arr[i] <= arr[j])
+    {
+        isTop = false;
+    }
+    if (isTop)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+Console.WriteLine(arr[arr.Length - 1]);
