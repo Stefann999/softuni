ffb6ebbc3a6e3d9626ff18ccd7fafe20e61b0140
Stefann999 <stefchog25@gmail.com>
2023-02-10 23:03:44 +0200

    Added ex. 6, 7, 8

---
+++ Exercise-Methods/6. Middle Characters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _6._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string text = Console.ReadLine();
+            
+            middleCharFinder(text);
+
+
+            static void middleCharFinder(string text)
+            {
+                int lenght = text.Length;
+
+                if (lenght % 2 == 0)
+                {
+                    int firstIndex = lenght / 2 - 1;
+                    int secondIndex = lenght / 2;
+
+
+                    Console.WriteLine($"{text[firstIndex]}{text[secondIndex]}");
+                }
+                else
+                {
+                    double n = lenght / 2 - 0.5;
+                    int index = (int)n;
+                    Console.WriteLine(text[index]);
+                }
+            }
+        }
+    }
+}
---
+++ Exercise-Methods/8. Factorial Division/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static decimal GetFactorial(decimal n)
+    {
+        decimal result = 1m;
+        for (int i = 1; i <= n; i++) result *= i;
+        return result;
+    }
+
+    static decimal FactorialDivision(decimal a, decimal b)
+    {
+        return GetFactorial(a) / GetFactorial(b);
+    }
+
+    static void Main()
+    {
+        decimal first = decimal.Parse(Console.ReadLine());
+        decimal second = decimal.Parse(Console.ReadLine());
+        Console.WriteLine($"{FactorialDivision(first, second):f2}");
+    }
+}
