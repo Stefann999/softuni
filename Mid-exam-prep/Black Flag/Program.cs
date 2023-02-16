94927b378a6d25beecc3905b6ef35db0f05a1a56
Stefann999 <stefchog25@gmail.com>
2023-02-16 20:32:23 +0200

    Added 1, 2 tasks

---
+++ Mid-exam-prep/Black Flag/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+int daysOfPlunder = int.Parse(Console.ReadLine());
+int dailyPlunder = int.Parse(Console.ReadLine());
+double expectedPlunder = double.Parse(Console.ReadLine());
+
+int thirdDayCnt = 0;
+int fifthDayCnt = 0;
+double gainedPlunder = 0;
+double dbDaily = (double)dailyPlunder;
+
+for (int i = 0; i < daysOfPlunder; i++)
+{
+    gainedPlunder = gainedPlunder + dailyPlunder;
+	thirdDayCnt++;
+	fifthDayCnt++;
+
+	if (thirdDayCnt == 3)
+	{
+		if (fifthDayCnt == 5)
+		{
+			gainedPlunder = gainedPlunder + dbDaily * 0.5;
+			gainedPlunder = gainedPlunder - gainedPlunder * 0.3;
+			thirdDayCnt = 0;
+			fifthDayCnt = 0;
+		}
+		else
+		{
+			gainedPlunder = gainedPlunder + dbDaily * 0.5;
+			thirdDayCnt = 0;
+		}
+	}
+	else if (fifthDayCnt == 5)
+	{
+		gainedPlunder = gainedPlunder - gainedPlunder * 0.3;
+		fifthDayCnt = 0;
+	}
+}
+if (gainedPlunder >= expectedPlunder)
+{
+	Console.WriteLine($"Ahoy! {gainedPlunder:f2} plunder gained.");
+}
+else
+{
+	double percentComplete = 100 * gainedPlunder / expectedPlunder;
+    Console.WriteLine($"Collected only {percentComplete:f2}% of the plunder.");
+}
---
+++ Mid-exam-prep/Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+double avg = Queryable.Average(numbers.AsQueryable());
+List<int> overAvg = new List<int>();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+	if (numbers[i] > avg)
+	{
+		overAvg.Add(numbers[i]);
+	}
+}
+int numsCnt = overAvg.Count();
+
+if (numsCnt < 5 && numsCnt > 0)
+{
+	overAvg.Sort();
+	overAvg.Reverse();
+	foreach (var item in overAvg)
+	{
+		Console.Write(item + " ");
+	}
+}
+else if (numsCnt >= 5)
+{
+	overAvg.Sort();
+	overAvg.Reverse();
+	
+	for (int i = 0; i < 5; i++)
+	{
+		Console.Write(overAvg[i] + " ");
+	}
+}
+else if (numsCnt == 0)
+{
+	Console.WriteLine("No");
+}
