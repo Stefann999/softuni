b611bca52035869a34033e772693ec1ac6828dc0
Stefann999 <stefchog25@gmail.com>
2023-04-01 15:13:34 +0300

    Added 2 projects

---
+++ Final-exam-prep/Fancy Barcodes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Principal;
+using System.Text.RegularExpressions;
+
+int n = int.Parse(Console.ReadLine());
+string input = Console.ReadLine();
+
+string pattern = @"(@)(#)+(?<first>[A-Z])(?<second>[A-Za-z-0-9]{4,})(?<third>[A-Z])\1(#)+$";
+
+for (int i = 0; i < n; i++)
+{
+    Match match = Regex.Match(input, pattern);
+
+    if (match.Success)
+    {
+        string firstGroup = match.Groups["first"].Value;
+        string secondGroup = match.Groups["second"].Value;
+        string thirdGroup = match.Groups["third"].Value;
+
+        List<int> nums = new List<int>();
+
+        int asInt = 0;
+        if (char.IsDigit(firstGroup[0]))
+        {
+            asInt = int.Parse(firstGroup[0].ToString());
+            nums.Add(asInt);
+        }
+        for (int j = 0; j < secondGroup.Length; j++)
+        {
+            if (char.IsDigit(secondGroup[j]))
+            {
+                asInt = int.Parse(secondGroup[j].ToString());
+                nums.Add(asInt);
+            }
+        }
+        if (char.IsDigit(thirdGroup[0]))
+        {
+            asInt = int.Parse(thirdGroup[0].ToString());
+            nums.Add(asInt);
+        }
+
+        if (nums.Count == 0)
+        {
+            Console.WriteLine("Product group: 00");
+        }
+        else
+        {
+            Console.WriteLine($"Product group: {string.Join(string.Empty, nums)}");
+        }
+
+        nums = new List<int>();
+        input = Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Invalid barcode");
+        input = Console.ReadLine();
+    }
+}
---
+++ Final-exam-prep/Heroes of Code and Logic VII/Program.cs
@@ -0,0 +1,88 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, string[]> heroes = new Dictionary<string, string[]>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] heroesInfo = Console.ReadLine().Split();
+    string[] value = new string[] { heroesInfo[1], heroesInfo[2] };
+    heroes.Add(heroesInfo[0], value);
+}
+
+string[] cmdArgs = Console.ReadLine().Split(" - ");
+string[] temp = null;
+
+while (cmdArgs[0] != "End")
+{
+    if (cmdArgs[0] == "CastSpell")
+    {
+        temp = heroes[cmdArgs[1]];
+        if (int.Parse(temp[1]) >= int.Parse(cmdArgs[2]))
+        {
+            int manaLeft = int.Parse(temp[1]) - int.Parse(cmdArgs[2]);
+            temp[1] = manaLeft.ToString();
+            heroes[cmdArgs[1]] = temp;
+            Console.WriteLine($"{cmdArgs[1]} has successfully cast {cmdArgs[3]} and now has {manaLeft} MP!");
+            temp = null;
+        }
+        else
+        {
+            Console.WriteLine($"{cmdArgs[1]} does not have enough MP to cast {cmdArgs[3]}!");
+        }
+    }
+
+    else if (cmdArgs[0] == "TakeDamage")
+    {
+        temp = heroes[cmdArgs[1]];
+        int hpLeft = int.Parse(temp[0]) - int.Parse(cmdArgs[2]);
+        if (hpLeft > 0)
+        {
+            temp[0] = hpLeft.ToString();
+            heroes[cmdArgs[1]] = temp;
+            Console.WriteLine($"{cmdArgs[1]} was hit for {cmdArgs[2]} HP by {cmdArgs[3]} and now has {temp[0]} HP left!");
+            temp = null;
+        }
+        else
+        {
+            heroes.Remove(cmdArgs[1]);
+            Console.WriteLine($"{cmdArgs[1]} has been killed by {cmdArgs[3]}!");
+        }
+    }
+
+    else if (cmdArgs[0] == "Recharge")
+    {
+        temp = heroes[cmdArgs[1]];
+        int result = int.Parse(temp[1]) + int.Parse(cmdArgs[2]);
+        if (result > 200)
+        {
+            result = 200;
+        }
+        int used = result - int.Parse(temp[1]);
+        temp[1] = result.ToString();
+        heroes[cmdArgs[1]] = temp;
+        Console.WriteLine($"{cmdArgs[1]} recharged for {used} MP!");
+        temp = null;
+    }
+
+    else if (cmdArgs[0] == "Heal")
+    {
+        temp = heroes[cmdArgs[1]];
+        int result = int.Parse(temp[0]) + int.Parse(cmdArgs[2]);
+        if (result > 100)
+        {
+            result = 100;
+        }
+        int used = result - int.Parse(temp[0]);
+        temp[0] = result.ToString();
+        heroes[cmdArgs[1]] = temp;
+        Console.WriteLine($"{cmdArgs[1]} healed for {used} HP!");
+    }
+    cmdArgs = Console.ReadLine().Split(" - ");
+}
+
+foreach (var item in heroes)
+{
+    Console.WriteLine(item.Key);
+    Console.WriteLine($" HP: {item.Value[0]}");
+    Console.WriteLine($" MP: {item.Value[1]}");
+}
