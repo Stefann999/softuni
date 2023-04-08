3d77fe49b1c65ac720a8ad9f73ed5641f5ad6404
Stefann999 <stefchog25@gmail.com>
2023-04-08 22:33:37 +0300

    Added 7th, 8th and 9th problems

---
+++ Advanced/Exercise-Stacks-Queues/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,69 @@
+﻿string input = Console.ReadLine();
+
+Stack<string> stack = new Stack<string>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '{' || input[i] == '[' || input[i] == '(')
+    {
+        stack.Push(input[i].ToString());
+    }
+    else if (input[i] == '}')
+    {
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+
+        if (stack.Peek() == "{")
+        {
+            stack.Pop();
+        }
+        else
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+    }
+
+    else if (input[i] == ']')
+    {
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+
+        if (stack.Peek() == "[")
+        {
+            stack.Pop();
+        }
+        else
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+    }
+
+    else if (input[i] == ')')
+    {
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+
+        if (stack.Peek() == "(")
+        {
+            stack.Pop();
+        }
+        else
+        {
+            Console.WriteLine("NO");
+            return;
+        }
+    }
+}
+
+ Console.WriteLine("YES");
