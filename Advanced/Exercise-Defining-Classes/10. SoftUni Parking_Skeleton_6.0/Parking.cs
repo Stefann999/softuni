30376d3c5c5df27be156acd708da45b97139c5d4
Stefann999 <stefchog25@gmail.com>
2023-04-14 22:39:29 +0300

    Added 10th project

---
+++ Advanced/Exercise-Defining-Classes/10. SoftUni Parking_Skeleton_6.0/Parking.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            cars = new();
+            this.capacity = capacity;
+        }
+
+        public int Count { get { return this.cars.Count; } }
+
+
+        public string AddCar(Car car)
+        {
+
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                string message = "Car with that registration number, already exists!";
+                return message;
+            }
+
+            else if (cars.Count == capacity)
+            {
+                string message = "Parking is full!";
+                return message;
+            }
+
+            else
+            {
+                cars.Add(car.RegistrationNumber, car);
+                string message = $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+                return message;
+            }
+        }
+
+
+        public string RemoveCar(string RegistrationNumber)
+        {
+
+            if (!cars.ContainsKey(RegistrationNumber))
+            {
+                string message = "Car with that registration number, doesn't exist!";
+                return message;
+            }
+
+            else
+            {
+                cars.Remove(RegistrationNumber);
+                string message = $"Successfully removed {RegistrationNumber}";
+                return message;
+            }
+        }
+
+
+        public Car GetCar(string RegistrationNumber)
+        {
+            Car car = cars[RegistrationNumber];
+            return car;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            for (int i = 0; i < RegistrationNumbers.Count; i++)
+            {
+                if (cars.ContainsKey(RegistrationNumbers[i]))
+                {
+                    cars.Remove(RegistrationNumbers[i]);
+                }
+            }
+        }
+
+    }
+}
