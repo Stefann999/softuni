dbc1b394fe2e0dc56e5a2ab52cc9a735cf877eb4
Stefann999 <stefchog25@gmail.com>
2023-11-26 15:18:31 +0200

    Added 15 August 2022 exam - First task

---
+++ DB/Entity Framework Core/Exam-Prep/15 August 2022/01. Model Definition_Skeleton/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trucks.Data.Models
+{
+    public class Despatcher
+    {
+        public Despatcher()
+        {
+            Trucks = new HashSet<Truck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Position { get; set; } = null!;
+
+        public virtual ICollection<Truck> Trucks { get; set; }
+    }
+}
---
+++ DB/Entity Framework Core/Exam-Prep/15 August 2022/01. Model Definition_Skeleton/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.Data.Models
+{
+    public class Truck
+    {
+        public Truck()
+        {
+            ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]{2}\\d{4}[A-Z]{2}$")]
+        public string RegistrationNumber { get; set; } = null!;
+
+        [Required]
+        public string VinNumber { get; set; } = null!;
+
+        [Required]
+        public int TankCapacity { get; set; }
+
+        [Required]
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        [Required]
+        public MakeType MakeType { get; set;}
+
+        [ForeignKey(nameof(Despatcher))]
+        public int DespatcherId { get; set; }
+
+        public Despatcher Despatcher { get; set; } = null!;
+
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; }
+    }
+}
